644f45adf81687d669b92af5570d247a2253cecb
Pierre Casco <pierre@Pierres-MBP.home>
2019-03-05 22:59:59 -0500

    Adding team info and starting plots

--- IST687 final project.R
+++ IST687 final project.R
@@ -2,6 +2,7 @@ library('Lahman')
 library('dplyr')
 library('ggplot2')
 library('sqldf')
+library('reshape2')
 
 #Get 30 greatest salaries
 playerSalary <- head(Salaries[order(-Salaries$salary),],30)
@@ -13,11 +14,11 @@ playerSalary <- merge(playerSalary,Master, by="playerID")
 playerSalary <- playerSalary[,colSums(is.na(playerSalary))<nrow(playerSalary)]
 
 #Create dataframe for top 5 paid players
-kershaw <- sqldf("Select * from playerSalary where playerID = 'kershcl01'")
-arod <- sqldf("Select * from playerSalary where playerID = 'rodrial01'")
-greinke <- sqldf("Select * from playerSalary where playerID = 'greinza01'")
-price <- sqldf("Select * from playerSalary where playerID = 'priceda01'")
-cabrera <- sqldf("Select * from playerSalary where playerID = 'cabremi01'")
+kershaw <- sqldf("Select * from Salaries where playerID = 'kershcl01'")
+arod <- sqldf("Select * from Salaries where playerID = 'rodrial01'")
+greinke <- sqldf("Select * from Salaries where playerID = 'greinza01'")
+price <- sqldf("Select * from Salaries where playerID = 'priceda01'")
+cabrera <- sqldf("Select * from Salaries where playerID = 'cabremi01'")
 
 #Plot salaries
 ggplot(kershaw,aes(x=yearID,y=salary)) + geom_line()
@@ -25,3 +26,17 @@ ggplot(arod,aes(x=yearID,y=salary)) + geom_line()
 ggplot(greinke,aes(x=yearID,y=salary)) + geom_line()
 ggplot(price,aes(x=yearID,y=salary)) + geom_line()
 ggplot(cabrera,aes(x=yearID,y=salary)) + geom_line()
+
+#Get team records
+teamRecs <- sqldf("Select yearID,teamID,attendance,W from Teams")
+
+#Merge with team record and attendance
+kershaw <- merge(kershaw,teamRecs,by=c("teamID","yearID"))
+arod <- merge(arod,teamRecs,by=c("teamID","yearID"))
+greinke <- merge(greinke,teamRecs,by=c("teamID","yearID"))
+price <- merge(price,teamRecs,by=c("teamID","yearID"))
+cabrera <- merge(cabrera,teamRecs,by=c("teamID","yearID"))
+
+#Plot salary and team wins
+a1 <- ggplot(arod,aes(x=yearID)) + geom_line(aes(y=salary), colour='blue') 
+a1 <- a1 + geom_line(aes(y=W),colour='red') + scale_y_continuous(sec.axis = sec_axis(~./3e+07,name = "Wins"))
