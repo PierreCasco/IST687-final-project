4c45ed7eaea4e5478517101d53217222a95c5480
Pierre Casco <pierre@Pierres-MBP.home>
2019-03-05 20:34:09 -0500

    Second commit

--- IST687 final project.R
+++ IST687 final project.R
@@ -1,4 +1,7 @@
 library('Lahman')
+library('dplyr')
+library('ggplot2')
+library('sqldf')
 
 #Get 30 greatest salaries
 playerSalary <- head(Salaries[order(-Salaries$salary),],30)
@@ -6,3 +9,19 @@ playerSalary <- head(Salaries[order(-Salaries$salary),],30)
 #Merge with player info to get name
 playerSalary <- merge(playerSalary,Master, by="playerID")
 
+#Remove NA columns
+playerSalary <- playerSalary[,colSums(is.na(playerSalary))<nrow(playerSalary)]
+
+#Create dataframe for 5 top paid players
+kershaw <- sqldf("Select * from Salaries where playerID = 'kershcl01'")
+arod <- sqldf("Select * from Salaries where playerID = 'rodrial01'")
+greinke <- sqldf("Select * from Salaries where playerID = 'greinza01'")
+price <- sqldf("Select * from Salaries where playerID = 'priceda01'")
+cabrera <- sqldf("Select * from Salaries where playerID = 'cabremi01'")
+
+#Plot salaries
+ggplot(kershaw,aes(x=yearID,y=salary)) + geom_line()
+ggplot(arod,aes(x=yearID,y=salary)) + geom_line()
+ggplot(greinke,aes(x=yearID,y=salary)) + geom_line()
+ggplot(price,aes(x=yearID,y=salary)) + geom_line()
+ggplot(cabrera,aes(x=yearID,y=salary)) + geom_line()
